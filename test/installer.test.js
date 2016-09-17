2ee4cd57b40b9689b753c42f9c92fcca5ebd7010
popomore <sakura9515@gmail.com>
2016-09-17 23:33:58 +0800

    test: test for unsafeVersions

---
+++ test/installer.test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const path = require('path');
+const rimraf = require('rimraf');
+const assert = require('power-assert');
+const execSync = require('child_process').execSync;
+const NodeInstaller = require('..').NodeInstaller;
+const fixtures = path.join(__dirname, 'fixtures');
+const config = require('../lib/config');
+
+
+describe('test/installer.test.js', function() {
+  let cwd;
+  beforeEach(function() {
+    if (cwd) {
+      rimraf.sync(path.join(cwd, 'node_modules'));
+    }
+  });
+  afterEach(function() {
+    if (cwd) {
+      rimraf.sync(path.join(cwd, 'node_modules'));
+    }
+  });
+
+  describe('unsafeVersions', function() {
+
+    it('should match unsafeVersions', function* () {
+      cwd = path.join(fixtures, 'install-node');
+      const nodeBinPath = path.join(cwd, 'node_modules/.bin/node');
+      const installer = new NodeInstaller({
+        cwd,
+        distUrl: config.nodeDistUrl,
+        version: '4.1.0',
+        unsafeVersions: {
+          '>= 1.0.0 < 4.4.4': '4.5.0',
+        },
+      });
+      yield installer.install();
+      assert(execSync(`${nodeBinPath} -v`).toString() === 'v4.5.0\n');
+    });
+  });
+
+});
